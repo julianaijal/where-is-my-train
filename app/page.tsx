0f5c92cac8f72c2370cc9dacb30b9dc7d12f08ff
Julian Aijal <julian@thenextweb.com>
2023-10-13 12:44:53 +0200

    get departure response from search

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
   const [searchQuery, setSearchQuery] = useState('');
 
   const handleSearch = (e) => {
     setSearchQuery(e.target.value);
+    getDeptarturesData('ASD');
+    console.log('spiderman');
   };
 
   return (
@@ -22,3 +24,34 @@ const Home = () => {
 };
 
 export default Home;
+
+const getDeptarturesData = async (stationCode:string) => {
+  const baseUrl =
+    'https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/departures';
+  const queryParams = {
+    lang: 'en',
+    station: stationCode,
+    maxJourneys: 10,
+  };
+
+  const queryString = Object.keys(queryParams)
+    .map((key) => key + '=' + queryParams[key])
+    .join('&');
+  const url = `${baseUrl}?${queryString}`;
+
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      'Ocp-Apim-Subscription-Key': 'ns-api',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Data received:', data);
+    })
+    .catch((error) => {
+      console.error('An error occurred:', error);
+    });
+
+  return getDeptarturesData;
+};
