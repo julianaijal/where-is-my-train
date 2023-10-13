d6f69d14c2432927d5d29b30cf66d3915b212a41
Julian Aijal <julian@thenextweb.com>
2023-10-13 12:51:50 +0200

    get station codes onclick searchbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,8 @@ import React, { useEffect, useState } from 'react';
 
 const Home = () => {
   const [searchQuery, setSearchQuery] = useState('');
-
   const handleSearch = (e) => {
     setSearchQuery(e.target.value);
-    getDeptarturesData('ASD');
-    console.log('spiderman');
   };
 
   return (
@@ -19,9 +16,35 @@ const Home = () => {
         value={searchQuery}
         onChange={handleSearch}
       />
+      <button onClick={getStationCodes(searchQuery)}>Search</button>
     </main>
   );
 };
 
 export default Home;
 
+const BASE_URL = 'https://gateway.apiportal.ns.nl';
+const SUBSCRIPTION_KEY = 'ns-api';
+
+const getStationCodes = async (stationName: string) => {
+  const headers = new Headers({
+    'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY,
+  });
+
+  const endpoint = `${BASE_URL}/reisinformatie-api/api/v2/stations?q=${stationName}`;
+
+  try {
+    const response = await fetch(endpoint, {
+      method: 'GET',
+      headers: headers,
+    });
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    } else {
+      console.error(`Error fetching station codes: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error(`There was a problem with the fetch operation: ${error}`);
+  }
+};
