9b83cdee6ae12adf205d15713f10801559052cc1
Julian Aijal <julian@thenextweb.com>
2023-10-13 12:46:10 +0200

    move departe fetch to vertrektijden

--- app/page.tsx
+++ app/page.tsx
@@ -25,33 +25,3 @@ const Home = () => {
 
 export default Home;
 
-const getDeptarturesData = async (stationCode:string) => {
-  const baseUrl =
-    'https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/departures';
-  const queryParams = {
-    lang: 'en',
-    station: stationCode,
-    maxJourneys: 10,
-  };
-
-  const queryString = Object.keys(queryParams)
-    .map((key) => key + '=' + queryParams[key])
-    .join('&');
-  const url = `${baseUrl}?${queryString}`;
-
-  fetch(url, {
-    method: 'GET',
-    headers: {
-      'Ocp-Apim-Subscription-Key': 'ns-api',
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log('Data received:', data);
-    })
-    .catch((error) => {
-      console.error('An error occurred:', error);
-    });
-
-  return getDeptarturesData;
-};
--- app/vertrektijden/page.tsx
+++ app/vertrektijden/page.tsx
@@ -7,3 +7,35 @@ const Departures = () => {
 };
 
 export default Departures;
+
+
+const getDeptarturesData = async (stationCode:string) => {
+    const baseUrl =
+      'https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/departures';
+    const queryParams = {
+      lang: 'en',
+      station: stationCode,
+      maxJourneys: 10,
+    };
+  
+    const queryString = Object.keys(queryParams)
+      .map((key) => key + '=' + queryParams[key])
+      .join('&');
+    const url = `${baseUrl}?${queryString}`;
+  
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Ocp-Apim-Subscription-Key': 'ns-api',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Data received:', data);
+      })
+      .catch((error) => {
+        console.error('An error occurred:', error);
+      });
+  
+    return getDeptarturesData;
+  };
