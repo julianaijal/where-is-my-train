d9611d55da9285607b9d9ec36136c6afd184750b
Julian Aijal <julian@thenextweb.com>
2023-10-13 12:27:28 +0200

    add search bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+'use client';
+import React, { useState } from 'react';
+
 const Home = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
   return (
     <main>
       <h1>Home</h1>
+      <input
+        type="text"
+        placeholder="Bestemming"
+        value={searchQuery}
+        onChange={handleSearch}
+      />
     </main>
   );
 };
