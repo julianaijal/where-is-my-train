a441e064a56090fea9d6d23d50f25f562af5925a
Julian Aijal <julian@thenextweb.com>
2023-10-15 20:34:39 +0200

    add station overview  api fetch in separate import

--- app/_utils/api.tsx
+++ app/_utils/api.tsx
@@ -1,33 +1,59 @@
-export const fetchDepartureData = async (stationCode) => {
-    const baseUrl =
-      'https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/departures';
-    const queryParams = {
-      lang: 'en',
-      station: stationCode,
-      maxJourneys: 10,
-    };
-  
-    const queryString = Object.keys(queryParams)
-      .map((key) => key + '=' + queryParams[key])
-      .join('&');
-    const url = `${baseUrl}?${queryString}`;
-  
-    try {
-      const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'Ocp-Apim-Subscription-Key': 'ns-api',
-        },
-      });
-  
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-  
+// to do: move token
+const BASE_URL = 'https://gateway.apiportal.ns.nl';
+const SUBSCRIPTION_KEY = 'ns-api';
+
+export const getStationCodes = async (stationName) => {
+  const headers = new Headers({
+    'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY,
+  });
+
+  const endpoint = `${BASE_URL}/reisinformatie-api/api/v2/stations?q=${stationName}`;
+
+  try {
+    const response = await fetch(endpoint, {
+      method: 'GET',
+      headers: headers,
+    });
+    if (response.ok) {
       const data = await response.json();
-      return data.payload.departures;
-    } catch (error) {
-      throw new Error('An error occurred: ' + error.message);
+      return data.payload;
+    } else {
+      throw new Error(`Error fetching station codes: ${response.statusText}`);
     }
+  } catch (error) {
+    throw new Error(`There was a problem with the fetch operation: ${error}`);
+  }
+};
+
+export const fetchDepartureData = async (stationCode) => {
+  const baseUrl =
+    'https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/departures';
+  const queryParams = {
+    lang: 'en',
+    station: stationCode,
+    maxJourneys: 10,
   };
-  
+
+  const queryString = Object.keys(queryParams)
+    .map((key) => key + '=' + queryParams[key])
+    .join('&');
+  const url = `${baseUrl}?${queryString}`;
+
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Ocp-Apim-Subscription-Key': 'ns-api',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    return data.payload.departures;
+  } catch (error) {
+    throw new Error('An error occurred: ' + error.message);
+  }
+};
--- app/page.tsx
+++ app/page.tsx
@@ -4,46 +4,29 @@ import { FC } from 'react';
 import { IStation } from './interfaces/interfaces';
 import { useRouter } from 'next/navigation';
 import styles from './styles/Home.module.scss';
+import { getStationCodes } from './_utils/api';
 
 const Home: FC<IStation> = () => {
   const router = useRouter();
   const [searchQuery, setSearchQuery] = useState('');
   const [stationCodes, setStationCodes] = useState('');
+
   const handleSearch = (e) => {
     setSearchQuery(e.target.value);
   };
 
-  const BASE_URL = 'https://gateway.apiportal.ns.nl';
-  const SUBSCRIPTION_KEY = 'ns-api';
-
-  const getStationCodes = async (stationName: string) => {
-    const headers = new Headers({
-      'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY,
-    });
-
-    const endpoint = `${BASE_URL}/reisinformatie-api/api/v2/stations?q=${stationName}`;
-
+  const handleGetStationCodes = async () => {
     try {
-      const response = await fetch(endpoint, {
-        method: 'GET',
-        headers: headers,
-      });
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data.payload);
-        setStationCodes(data.payload);
-      } else {
-        console.error(`Error fetching station codes: ${response.statusText}`);
-      }
+      const codes = await getStationCodes(searchQuery);
+      setStationCodes(codes);
     } catch (error) {
-      console.error(`There was a problem with the fetch operation: ${error}`);
+      console.error(error.message);
     }
   };
 
   return (
     <main className={styles.main}>
       <section className={styles.searchElement}>
-        {' '}
         <h2 className={styles.subtitle}>Stations informatie</h2>
         <h1 className={styles.title}>Zoek je station</h1>
         <input
@@ -52,7 +35,7 @@ const Home: FC<IStation> = () => {
           value={searchQuery}
           onChange={handleSearch}
         />
-        <button onClick={() => getStationCodes(searchQuery)}>Search</button>
+        <button onClick={handleGetStationCodes}>Search</button>
         {stationCodes ? (
           <div>
             {stationCodes.map((stationCode: IStation, i: number) => (
