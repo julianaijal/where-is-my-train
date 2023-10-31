12602c1c9675e933e22a2387d344569918eac33e
Julian Aijal <julian@thenextweb.com>
2023-10-31 17:56:41 +0100

    add spinner while fetching

--- app/vertrektijden/[id]/page.tsx
+++ app/vertrektijden/[id]/page.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import styles from './../../styles/Departures.module.scss';
 import { fetchDepartureData } from './../../_utils/api';
 import Table from '@/app/_components/_departures/Table';
+import Loader from '@/app/_components/_partials/Loader';
 
 const Page = ({ params }: { params: { id: string } }) => {
   const [stationDepartureTimes, setStationDepartureTimes] = useState<
@@ -28,11 +29,7 @@ const Page = ({ params }: { params: { id: string } }) => {
   return (
     <section className={styles.main}>
       <h1 className={styles.title}>Vertrektijden</h1>
-      {loading ? (
-        'Loading...'
-      ) : (
-          <Table departures={stationDepartureTimes}/>
-      )}
+      {loading ? <Loader /> : <Table departures={stationDepartureTimes} />}
     </section>
   );
 };
