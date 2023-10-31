524f06f01d8df31dcce08726e19fe0dc3fea2da3
Julian Aijal <julian@thenextweb.com>
2023-10-31 21:08:16 +0100

    fix table nesting

--- app/_components/_departures/Table.tsx
+++ app/_components/_departures/Table.tsx
@@ -4,19 +4,23 @@ import { IDepartures } from '@/app/interfaces/interfaces';
 const Table = ({ departures }: IDepartures) => {
   return (
     <table>
-      <tr>
-        <th>Vertrektijd</th>
-        <th>Naar</th>
-        <th>Platform</th>
-      </tr>
-      {departures.map(({ direction, plannedDateTime, actualTrack }, i) => (
-        <TrainEntry
-          direction={direction}
-          departure={plannedDateTime}
-          track={actualTrack}
-          key={i}
-        />
-      ))}
+      <thead>
+        <tr>
+          <th>Vertrektijd</th>
+          <th>Naar</th>
+          <th>Platform</th>
+        </tr>
+      </thead>
+      <tbody>
+        {departures.map(({ direction, plannedDateTime, actualTrack }, i) => (
+          <TrainEntry
+            direction={direction}
+            departure={plannedDateTime}
+            track={actualTrack}
+            key={i}
+          />
+        ))}
+      </tbody>
     </table>
   );
 };
