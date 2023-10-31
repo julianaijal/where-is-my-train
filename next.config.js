06345e8457670eed899fd57f08cb0b3ad4275be7
Julian Aijal <julian@thenextweb.com>
2023-10-31 21:39:45 +0100

    enable webpack build worker

--- next.config.js
+++ next.config.js
@@ -1,15 +1,17 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
+const withPWA = require('next-pwa')({
+  dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === 'development',
 });
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        NS_API:  process.env.API_KEY,
-      },
-    
-}
+  experimental: {
+    webpackBuildWorker: true,
+  },
+  env: {
+    NS_API: process.env.API_KEY,
+  },
+};
 
 module.exports = withPWA(nextConfig);
