e6a5293ebecedb5ab9df0c67db7e70d4ba8b5c22
Julian Aijal <julian@thenextweb.com>
2023-11-05 18:26:25 +0100

    style table

--- app/_components/_departures/Table.tsx
+++ app/_components/_departures/Table.tsx
@@ -1,24 +1,23 @@
-import TrainEntry from '@/app/_components/_departures/TrainEntry';
 import { IDepartures } from '@/app/interfaces/interfaces';
+import styles from './../../styles/Departures.module.scss';
 
 const Table = ({ departures }: IDepartures) => {
   return (
-    <table>
+    <table className={styles.departuresTable}>
       <thead>
         <tr>
-          <th>Vertrektijd</th>
-          <th>Naar</th>
-          <th>Platform</th>
+          <th className={styles.tableHeader}>Vertrektijd</th>
+          <th className={styles.tableHeader}>Naar</th>
+          <th className={styles.tableHeader}>Platform</th>
         </tr>
       </thead>
       <tbody>
         {departures.map(({ direction, plannedDateTime, actualTrack }, i) => (
-          <TrainEntry
-            direction={direction}
-            departure={plannedDateTime}
-            track={actualTrack}
-            key={i}
-          />
+          <tr key={i} className={i % 2 === 0 ? styles.evenRow : styles.oddRow}>
+            <td className={styles.tableData}>{plannedDateTime}</td>
+            <td className={styles.tableData}>{direction}</td>
+            <td className={styles.tableData}>{actualTrack}</td>
+          </tr>
         ))}
       </tbody>
     </table>
