98da50ab3849495c75247a809f7b833a498d30cf
Julian Aijal <julian@thenextweb.com>
2023-11-05 18:41:22 +0100

    removem tr include and format time in same file

--- app/_components/_departures/Table.tsx
+++ app/_components/_departures/Table.tsx
@@ -1,6 +1,15 @@
 import { IDepartures } from '@/app/interfaces/interfaces';
 import styles from './../../styles/Departures.module.scss';
 
+const formatTime = (dateString: string | undefined) => {
+  if (dateString) {
+    const date = new Date(dateString);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    return `${hours}:${minutes}`;
+  }
+};
+
 const Table = ({ departures }: IDepartures) => {
   return (
     <table className={styles.departuresTable}>
@@ -14,7 +23,7 @@ const Table = ({ departures }: IDepartures) => {
       <tbody>
         {departures.map(({ direction, plannedDateTime, actualTrack }, i) => (
           <tr key={i} className={i % 2 === 0 ? styles.evenRow : styles.oddRow}>
-            <td className={styles.tableData}>{plannedDateTime}</td>
+            <td className={styles.tableData}>{formatTime(plannedDateTime)}</td>
             <td className={styles.tableData}>{direction}</td>
             <td className={styles.tableData}>{actualTrack}</td>
           </tr>
