f2a679169370a3bc81abb99639fee6beddd7a026
Julian Aijal <julian.aijal@gmail.com>
2026-09-08 15:53:06 +0200

    Fix Next.js 16 Turbopack configuration

--- next.config.js
+++ next.config.js
@@ -6,9 +6,7 @@ const withPWA = require('next-pwa')({
 });
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    webpackBuildWorker: true,
-  },
+  turbopack: {},
   env: {
     NS_API: process.env.API_KEY,
   },
